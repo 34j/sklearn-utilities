e79e22b00b9149530f96d959526f85addc2d236b
34j <55338215+34j@users.noreply.github.com>
2023-10-26 18:19:12 +0900

    feat(transformed_target_estimator): add inverse_transform_separtely parameter

--- src/sklearn_utilities/proba/transformed_target_estimator.py
+++ src/sklearn_utilities/proba/transformed_target_estimator.py
@@ -36,10 +36,15 @@ class TransformedTargetEstimatorVar(
     """TransformTargetRegressor with std/var support."""
 
     def __init__(
-        self, estimator: TEstimator, transformer: TTransformer = IdTransformer()
+        self,
+        estimator: TEstimator,
+        *,
+        transformer: TTransformer = IdTransformer(),
+        inverse_transform_separately: bool = False,
     ) -> None:
         super().__init__(estimator)
         self.transformer = transformer
+        self.inverse_transform_separately = inverse_transform_separately
 
     def fit(self, X: TX, y: TY, **fit_params: Any) -> Self:
         y = _parse_2d(self.transformer.fit_transform)(y)
@@ -49,9 +54,14 @@ class TransformedTargetEstimatorVar(
     def predict(self, X: TX, **predict_params: Any) -> TY | tuple[TY, TY]:
         if predict_params.get("return_std", False):
             pred, pred_std = self.estimator.predict(X, **predict_params)
-            return _parse_2d(self.transformer.inverse_transform)(X), _parse_2d(
-                self.transformer.inverse_transform
-            )(pred_std, return_std=True)
+            if self.inverse_transform_separately:
+                return _parse_2d(self.transformer.inverse_transform)(pred), _parse_2d(
+                    self.transformer.inverse_transform
+                )(pred_std, return_std=True)
+            else:
+                return _parse_2d(self.transformer.inverse_transform)(
+                    (pred, pred_std), return_std=True
+                )
         pred = self.estimator.predict(X, **predict_params)
         return _parse_2d(self.transformer.inverse_transform)(pred)
 
